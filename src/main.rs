4661dacd0f3cc5f3dde582a54b2de78007aa5567
Yuriy Lyubchenko <yuriy.lyubchenko@gentlecode.com>
2026-07-15 13:12:13 +0300

    add bevy, wasm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "potential_doodle".into(),
+                fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+#[derive(Component)]
+struct Spinner;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Sprite::from_color(Color::srgb(0.9, 0.4, 0.3), Vec2::splat(150.0)),
+        Spinner,
+    ));
+}
+
+fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Spinner>>) {
+    for mut transform in &mut query {
+        transform.rotate_z(time.delta_secs());
+    }
 }
